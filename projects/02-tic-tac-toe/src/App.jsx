9d9d907b96ba0ac7d8fb2f911ffac62505ce4268
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-03-25 13:07:03 +0100

    Tic-tac-toe v0.1.0

---
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+
+const Square = ({children, updateBoard, index}) => {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+      {
+        board.map((Square, index => {
+          return (
+            <Square
+             key={index}
+             index={index}
+             >
+              {board[index]}
+             </Square>
+          )
+        }))
+      }
+      </section>
+    </main>
+  )
+}
+
+export default App
