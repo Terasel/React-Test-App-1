ec83c679441d3d0c3bcc2e9e7713737ffd290474
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-03-26 13:15:34 +0100

    Tic-tac-toe v0.2.0

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import _, { map } from 'underscore'
 
 const TURNS = {
   X: 'x',
@@ -7,9 +8,15 @@ const TURNS = {
 
 
 
-const Square = ({children, updateBoard, index}) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return (
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -19,22 +26,37 @@ function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
   const [turn, setTurn] = useState(TURNS.X)
 
+  const updateBoard = (index) => {
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+  }
+
   return (
     <main className="board">
       <h1>Tic tac toe</h1>
       <section className="game">
-      {
-        board.map((Square, index => {
-          return (
-            <Square
-             key={index}
-             index={index}
-             >
-              {board[index]}
-             </Square>
-          )
-        }))
-      }
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}</Square>
       </section>
     </main>
   )
