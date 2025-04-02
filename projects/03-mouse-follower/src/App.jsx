be444bd93dbe63d7f8bb893e6cce6c2a7058c237
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-04-02 13:47:54 +0200

    Mouse follower v0.1.0

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -2,17 +2,44 @@ import { useEffect, useState } from "react"
 
 function App() {
   const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
   useEffect(() => {
-    console.log('effect')
-  })
+    console.log('effect', { enabled })
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <>
-      <h3>Project 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '3px solid #ff0',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Deactivate' : 'Activate'} follow pointer
       </button>
-    </>
+    </main>
   )
 }
 
