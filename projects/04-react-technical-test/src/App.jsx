e564d7e171be41afc5f8cc95af2245da2166e2a1
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-04-10 14:04:53 +0200

    Technical test simulacrum v0.3.0

--- projects/04-react-technical-test/src/App.jsx
+++ projects/04-react-technical-test/src/App.jsx
@@ -1,22 +1,34 @@
 import { useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?
-size=50&color=red&json=true`
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWords}?
+// size=50&color=red&json=true`
 
 export function App() {
-    const [fact, setFact] = useState('lorem ipsum kitten powah')
+    const [fact, setFact] = useState()
 
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
             .then(res => res.json())
-            .then(data => setFact(data.fact))
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+
+                const firstWords = fact.split(' ', 3)
+
+                fetch(`https://cataas.com/cat/says/${firstWords}?size=50&color=red&
+                json=true`)
+                .then(res=>res.json())
+                .then(response=>{
+                    
+                })
+            })
     }, [])
 
     return (
         <main>
             <h1>Kitten app</h1>
-            <p>{fact}</p>
+            {fact && <p>{fact}</p>}
         </main>
     )
 }
