13a6041ac925a8c287c28f78989995811ac9d810
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-04-14 15:17:21 +0200

    Technical test simulacrum v1.1.0

--- projects/04-react-technical-test/src/App.jsx
+++ projects/04-react-technical-test/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import { getRandomFact } from './services/facts';
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWords}?
 // size=50&color=red&json=true`
 
@@ -9,17 +9,14 @@ export function App() {
     const [fact, setFact] = useState()
     const [imageUrl, setimageUrl] = useState()
 
+
+
     useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
-            .then(data => {
-                const { fact } = data
-                setFact(fact)
-            })
+        getRandomFact().then(setFact)
     }, [])
 
     useEffect(() => {
-        if(!fact) return
+        if (!fact) return
 
         const firstWords = fact.split(' ', 3).join(' ')
 
@@ -32,9 +29,17 @@ export function App() {
             })
     }, [fact])
 
+    const handleClick = async () => {
+        const newFact = await getRandomFact()
+        setFact(newFact)
+    }
+
     return (
         <main>
             <h1>Kitten app</h1>
+
+            <button onClick={handleClick}>Get new fact</button>
+
             <section>
                 {fact && <p>{fact}</p>}
                 {imageUrl && <img src={imageUrl} alt={`Image extracted using the first 
