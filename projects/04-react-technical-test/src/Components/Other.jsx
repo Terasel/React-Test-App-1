7a93a7595c0128a43b38a5f5deaff7363e485329
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-04-17 15:37:40 +0200

    Technical test simulacrum v1.4.0 + playwright install

---
+++ projects/04-react-technical-test/src/Components/Other.jsx
@@ -0,0 +1,11 @@
+import { useCatImage } from "../hooks/useCatImage"
+
+export function Other () {
+    const { imageUrl } = useCatImage({ fact: 'cat' })
+
+    return (
+        <>
+                {imageUrl && <img src={imageUrl}/>}
+        </>
+    )
+}
