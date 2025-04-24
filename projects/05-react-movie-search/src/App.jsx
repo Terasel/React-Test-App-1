3f69c6fc806b74b1ee32c89d897d1f532766833c
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-04-24 15:23:27 +0200

    Movie search test v0.1.2 + page titles

--- projects/05-react-movie-search/src/App.jsx
+++ projects/05-react-movie-search/src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
+import { Movies } from './components/Movies'
 import responseMovies from './mocks/with-results.json'
 // import withoutResults from './mocks/no-results.json'
 
 function App() {
   const movies = responseMovies.Search
-  const hasMovies = movies?.length > 0
 
   return (
     <div className='page'>
@@ -17,27 +17,9 @@ function App() {
       </header>
 
       <main>
-        {
-          hasMovies
-          ? (
-            <ul>
-              {
-              movies.map(movie => (
-                <li key={movie.imdbID}>
-                  <h3>{movie.Title}</h3>
-                  <p>{movie.Year}</p>
-                  <img src={movie.Poster} alt={movie.Title} />
-                </li>
-              ))
-              }
-            </ul>
-          )
-          : (
-            <p>No movies could be found</p>
-          )
-        }
+        <Movies movies={movies}/>
       </main>
-    </div>
+    </div >
   )
 }
 
