45e2b63f834e84d8e86f5a5c86ef2ff94e75705d
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-04-29 15:06:14 +0200

    Movie search test v0.2.1

--- projects/05-react-movie-search/src/App.jsx
+++ projects/05-react-movie-search/src/App.jsx
@@ -5,12 +5,19 @@ import { useMovies } from './hooks/useMovies'
 function App() {
   const { movies } = useMovies()
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const fields = new window.FormData(e.target)
+    const query = fields.get('query')
+    console.log(query)
+  }
+
   return (
     <div className='page'>
       <header>
         <h1>Movie search</h1>
-        <form className='form'>
-          <input type="text" placeholder='Avengers, Star Wars, The Matrix...' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input name='query' type="text" placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Search</button>
         </form>
       </header>
