6233018e060d6b6ebb2e247bf6f96e0e5a160c8d
Arnau Vidaña Garcia <arnauvidana95@gmail.com>
2025-04-30 22:09:21 +0200

    Movie search test v0.2.2

--- projects/05-react-movie-search/src/App.jsx
+++ projects/05-react-movie-search/src/App.jsx
@@ -1,25 +1,52 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const { movies } = useMovies()
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    const fields = new window.FormData(e.target)
-    const query = fields.get('query')
-    console.log(query)
+    console.log({ query })
   }
+
+  const handleChange = (e) => {
+    setQuery(e.target.value)
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      setError('You cannot search for a movie with no name')
+      return
+    }
+
+    if (query.match(/^\d+$/)) {
+      setError('You cannot search for a movie with only a number for a name')
+      return
+    }
+
+    if (query.length < 3) {
+      setError('A search must have at least 3 characters')
+      return
+    }
+
+    setError(null)
+  }, [query])
 
   return (
     <div className='page'>
       <header>
         <h1>Movie search</h1>
         <form className='form' onSubmit={handleSubmit}>
-          <input name='query' type="text" placeholder='Avengers, Star Wars, The Matrix...' />
+          <input onChange={handleChange} value={query} name='query' type="text"
+            placeholder='Avengers, Star Wars, The Matrix...' />
           <button type='submit'>Search</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
